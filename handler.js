9d7f72744520a1332e0ae01ff5a62c45e25bbd84
Aldmit <TPiBD@yandex.ru>
2019-07-17 12:25:17 +0300

    ~css ~js ~html

--- handler.js
+++ handler.js
@@ -4,17 +4,38 @@ var keysObj = "";
 var keyObj = "";
 
 var base = {};
+var genQuestion = "";
 
 $(document).ready(function(){
 
   function generateQuestion(){
-    keysBase = Object.keys(base);
+		keysBase = Object.keys(base);
     keyBase = Math.floor(Math.random() * (keysBase.length));
     
     keysObj = Object.keys(base[keysBase[keyBase]]);
     keyObj = Math.floor(Math.random() * (keysObj.length));
    // ключ keysObj[keyObj];
-   // значение base[keysBase[keyBase]][keysObj[keyObj]];
+	 // значение base[keysBase[keyBase]][keysObj[keyObj]];
+	  genQuestion = base[keysBase[keyBase]][keysObj[keyObj]];
+	}
+	function nextQuestion(){
+		if(!keysObj.length){
+			keysBase = Object.keys(base);
+			keyBase = Math.floor(Math.random() * (keysBase.length));
+			
+			keysObj = Object.keys(base[keysBase[keyBase]]);
+			keyObj = Math.floor(Math.random() * (keysObj.length));
+			genQuestion = base[keysBase[keyBase]][keysObj[0]];
+		}
+		else if(keysObj.length <= keyObj){
+			keyObj = 0;
+			genQuestion = base[keysBase[keyBase]][keysObj[keyObj]];
+			console.log(keysObj.length+" , "+keyObj);
+		}else{
+			genQuestion = base[keysBase[keyBase]][keysObj[keyObj++]];
+			console.log(keysObj.length+" , "+keyObj);
+		}
+		
   }
   
 //   Работа с меню
@@ -59,6 +80,20 @@ $(document).ready(function(){
 //   Работа со сладером
 
 	$('.b-main .i-nav_next').click(function(){
+		nextQuestion();
+		if($('.i-answer').hasClass('_show')){
+			$('.i-answer').toggleClass('_show');
+			$('.i-question').toggleClass('_show');
+		}
+		if($('.b-header .b-nav').hasClass('_show')) 
+		$('.b-header .b-nav').toggleClass('_show');
+		
+		$('.i-question').html(keysObj[keyObj]);
+		$('.i-answer').html(genQuestion);
+	});
+
+	
+	$('.b-header .i-random').click(function(){
 		generateQuestion();
 		if($('.i-answer').hasClass('_show')){
 			$('.i-answer').toggleClass('_show');
@@ -68,7 +103,7 @@ $(document).ready(function(){
 		$('.b-header .b-nav').toggleClass('_show');
 		
 		$('.i-question').html(keysObj[keyObj]);
-		$('.i-answer').html(base[keysBase[keyBase]][keysObj[keyObj]]);
+		$('.i-answer').html(genQuestion);
 	});
 
 });
